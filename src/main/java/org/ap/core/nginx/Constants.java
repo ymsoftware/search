717f061c1e15378875b1da957ad9c07d75fc8b0d
Yuri Metelkin <ymetelkin@yahoo.com>
2016-05-01 20:12:40 -0400

    Work in progress

--- src/main/java/org/ap/core/nginx/Constants.java
+++ src/main/java/org/ap/core/nginx/Constants.java
@@ -9,6 +9,9 @@ import static nginx.clojure.MiniConstants.CONTENT_TYPE;
  */
 public class Constants {
     public static final String REQUEST_BODY = "body";
+    public static final String REQUEST_URI = "uri";
+
+    public static final String API_SEARCH_URI = "/api/search";
 
     public static final ArrayMap JSON_CONTENT_TYPE = ArrayMap.create(CONTENT_TYPE, "text/json");
 }
